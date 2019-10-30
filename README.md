37438762de2a56588d0da99941d7034ef8a0afc2
svensp <sps@ipunkt.biz>
2019-10-30 14:40:08 +0100

    readme, slack-message as python script

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # docker-diskmonitor
+A docker container image to monitor the current usage percent of the partition
+mounted as /.
+
+Warnings will be posted to an incoming webhook in slack.
+
+## Configuration
+Configuration is done through environment variables. The following are
+available:
+- `SLACK_URL` REQUIRED url of the slack webhook
+- `SLACK_NOTIFY_USERS` comma separated list of users to notify whe
+- `SLEEP` time in between checks, defaults to 5m
+- `THRESHOLD` disk usage percent threshold to start warning defaults to 90
--- check.sh
+++ check.sh
@@ -1,20 +1,38 @@
 #!/bin/sh
 
 SLEEP="${SLEEP:-5m}"
-LIMIT="${LIMIT:-90}"
+THRESHOLD="${THRESHOLD:-90}"
 HOSTFILE="${HOSTFILE:-/etc/hostname}"
 HOSTNAME=$(cat $HOSTFILE)
+IS_WARNING="false"
 
 while true ; do
 	PERCENT=$(df / --output=pcent | tail -1 | tr -d %)
 
-	if [ "${PERCENT}" -gt "100" ] ; then
-		MESSAGE="disk on ${HOSTNAME} is ${PERCENT}% -> FULL"
-		slack-message -error "${MESSAGE}"
-	elif [ "${PERCENT}" -gt "${LIMIT}" ] ; then
-		MESSAGE="Current disk percent on ${HOSTNAME} is ${PERCENT}% and thus over the threshold of ${LIMIT}%"
-		echo ${MESSAGE}
-		slack-message -warning "${MESSAGE}"
+	if [ "${PERCENT}" -ge "100" ] ; then
+		if [ "${FULL_WARNING}" != "true" ] ; then
+			MESSAGE="disk on ${HOSTNAME} is ${PERCENT}% -> FULL"
+			echo ${MESSAGE}
+			slack-message -error "${MESSAGE}"
+		fi
+		FULL_WARNING="true"
+		PERCENT_WARNING="false"
+	elif [ "${PERCENT}" -ge "${THRESHOLD}" ] ; then
+
+		if [ "${PERCENT_WARNING}" != "true" ] ; then
+			MESSAGE="Current disk percent on ${HOSTNAME} is ${PERCENT}% and thus over the threshold of ${THRESHOLD}%"
+			echo ${MESSAGE}
+			slack-message -warning "${MESSAGE}"
+		fi
+		PERCENT_WARNING="true"
+	else
+		if [ "${PERCENT_WARNING}" = "true" ] ; then
+			MESSAGE="Current disk percent on ${HOSTNAME} fell bellow ${THRESHOLD}%"
+			echo ${MESSAGE}
+			slack-message -ok "${MESSAGE}"
+		fi
+		FULL_WARNING="false"
+		PERCENT_WARNING="false"
 	fi
 
 	sleep "${SLEEP}"
